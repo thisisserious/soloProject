c6d43df1aea0530eb420e3308a9e74224ad76048
Laura Abend <laura.l.abend@gmail.com>
2016-11-02 15:13:01 -0500

    add register angular-route placeholder

--- public/scripts/route.config.js
+++ public/scripts/route.config.js
@@ -2,6 +2,8 @@ angular.module('goodHelpApp')
        .config(function($routeProvider, $locationProvider) {
           $routeProvider.when('/home', {
             templateUrl: 'views/home.html'
+          }).when('/register', {
+            templateUrl: 'views/register.html'
           });
           $locationProvider.html5Mode(true);
         });
