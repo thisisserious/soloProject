2577996c39ee9c0881865efbab366bf52e31e647
Laura Abend <laura.l.abend@gmail.com>
2016-11-03 10:18:13 -0500

    add api, placeid, and key for googleAPI

--- public/scripts/index.service.js
+++ public/scripts/index.service.js
@@ -1,6 +1,11 @@
 angular.module('goodHelpApp')
        .service('goodHelpService', GoodHelpService);
 
-function GoodHelpService() {
+function GoodHelpService($http) {
   console.log('GoodHelpService loaded');
+  var api = 'https://maps.googleapis.com/maps/api/place/details/json';
+  var placeid = 'ChIJvbt3k5Azs1IRB-56L4TJn5M';
+  var key = 'AIzaSyAvxVdxhDqyVuMMiiKEcoqBAva5BOguGKw';
 }
+
+// + '?placeid=' + placeid + '&key=' + key <-- concatenation
