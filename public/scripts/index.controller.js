b264d97953b81478165fd84d2f2468d400048e99
Laura Abend <laura.l.abend@gmail.com>
2016-11-05 17:33:05 -0500

    add more info to search results; clear search input field on submit

--- public/scripts/index.controller.js
+++ public/scripts/index.controller.js
@@ -22,6 +22,7 @@ function GoodHelpController($http, goodHelpService) {
 
     goodHelpService.search(searchTerm)
     .then(function(results) {
+      controller.searchTerm = '';
       controller.searchedPlaces = results;
     });
   };
