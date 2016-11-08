beeb53fc6bb21a4d624493bc4116262f85449fd4
Laura Abend <laura.l.abend@gmail.com>
2016-11-08 13:43:19 -0600

    add mongo db POST functionality to submit & save reviews

--- public/scripts/index.controller.js
+++ public/scripts/index.controller.js
@@ -2,9 +2,9 @@ angular.module('goodHelpApp')
        .controller('GoodHelpController', GoodHelpController);
 
 function GoodHelpController($http, goodHelpService) {
-  // console.log('GoodHelpController loaded');
   var controller = this;
 
+  // retrieve data from mongo db
   controller.listOrg = function() {
     console.log('Listing organizations');
     $http.get('/orgs').then(function(response) {
@@ -16,19 +16,27 @@ function GoodHelpController($http, goodHelpService) {
     });
   };
 
+  // search for an organzation
   controller.searchFor = function(searchTerm) {
-    // console.log('Search term from index.html:', searchTerm);
     controller.searchTerm = '';
-
     goodHelpService.search(searchTerm)
     .then(function(results) {
       controller.searchedPlaces = results;
     });
   };
 
+  // create an organization's profile
   controller.createProfile = function(buttonClick) {
     console.log('buttonClick', buttonClick);
     controller.selectedProfile = buttonClick;
     console.log('controller', controller);
+  };
+
+  // post review to mongo db
+  controller.submitReview = function(review) {
+    var data = {review: controller.review};
+    console.log('review:', data);
+    $http.post('/orgs', data);
+    controller.review = '';
   };
 }
--- routes/orgs.js
+++ routes/orgs.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
 const Org = require('../models/organization');
 
+// get documents from mongo db
 router.get('/', function(req, res) {
   Org.find({}).then(function(dataFromTheDatabase) {
     console.log('Documents from mongo', dataFromTheDatabase);
     res.send(dataFromTheDatabase);
+  });
+});
+
+// post documents to mongo db
+router.post('/', function(req, res) {
+  var review = req.body.review;
+  console.log('post review:', review);
+  var saveReview = new Org({review: review});
+  console.log('saveReview:', saveReview);
+  saveReview.save().then(function() {
+    console.log('Saved a new review');
+    res.sendStatus(201);
   });
 });
 
